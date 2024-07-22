471ee4ec3b8b2590edad86c9ad582892f090727b
AlexItalbazar <a-egorov@bk.ru>
2024-07-22 15:56:17 +0300

    оптимизация, проверка ввода года

--- mylib.py
+++ mylib.py
@@ -10,20 +10,19 @@ class LibraryManagement:
     def load_db(self):
         try:
             with open(self.db_file_name, 'r', encoding='utf-8') as f:
-                self.db = json.load(f)
-                self.books = [book for book in self.db]
+                self.books = json.load(f)
         except FileNotFoundError:
             self.books = []
 
     def save_db(self):
         with open(self.db_file_name, 'w', encoding='utf-8') as f:
-            json.dump([book for book in self.books], f, ensure_ascii=False, indent=4)
+            json.dump(self.books, f, ensure_ascii=False, indent=4)
 
     def add_book(self):
         id = str(uuid.uuid4())
         title = input('Введите название книги: ')
         author = input('Введите автора книги: ')
-        year = input('Введите год издания книги: ')
+        year = self.input_year()
         status = 'в наличии'
         new_book = {
             'id': id, 'title': title, 'author': author, 'year': year, 'status': status,
@@ -35,78 +34,76 @@ class LibraryManagement:
 
     def delete_book(self):
         id_book = input('Введите идентификатор книги: ').strip()
-        for book in self.books:
-            if book['id'] == id_book:
-                self.books.remove(book)
-                self.save_db()
-                print(f'Книга "{book["title"]}" успешно удалена!')
-                return main()
-        print('Книга с таким идентификатором не найдена!')
+        book = next((b for b in self.books if b['id'] == id_book), None)
+        if book:
+            self.books.remove(book)
+            self.save_db()
+            print(f'Книга "{book["title"]}" успешно удалена!')
+        else:
+            print('Книга с таким идентификатором не найдена!')
         main()
 
     def find_book(self):
-        flag = False
+        search_options = {
+            '1': 'title',
+            '2': 'author',
+            '3': 'year'
+        }
         print('1 - Название книги', '2 - Автор книги', '3 - Год издания книги', sep='\n')
         action = input('Введите метод поиска: ').strip()
-        if action == '1':
-            title = input('Введите название книги: ').strip()
-            for book in self.books:
-                if title.lower() in book['title'].lower():
-                    flag = True
-                    print(f'Найдена книга "{book["title"]}" автора "{book["author"]}"')
-            if not flag:
-                print('Книга с таким названием не найдена!')
-
-        elif action == '2':
-            author = input('Введите автора книги: ').strip()
-            for book in self.books:
-                if author.lower() in book['author'].lower():
-                    flag = True
-                    print(f'Найдена книга "{book["title"]}" автора "{book["author"]}"')
-            if not flag:
-                print('Книга с таким названием не найдена!')
-
-        elif action == '3':
-            year = input('Введите год издания книги: ').strip()
-            for book in self.books:
-                if book['year'] == year:
-                    print(f'Найдена книга "{book["title"]}" автора "{book["author"]}"')
-                    flag = True
-            if not flag: (
-                print('Книга с таким годом издания не найдена!'))
-
-        else: print('Неверный ввод!')
+        if action in search_options:
+            if search_options[action] == 'year':
+                query = self.input_year()
+            else:
+                query = input(f'Введите {search_options[action]} книги: ').strip().lower()
+            found_books = [book for book in self.books if query in book[search_options[action]].lower()]
+            if found_books:
+                for book in found_books:
+                    print(f'id: {book["id"]}, название: {book["title"]}, автор: {book["author"]}, год: {book["year"]}, статус: {book["status"]}')
+            else:
+                print(f'Книга с таким {search_options[action]} не найдена!')
+        else:
+            print('Неверный ввод!')
         main()
 
     def show_all_books(self):
         for book in self.books:
             print(
-                f'id: {book["id"]}, название: {book["title"]}, автор: {book["author"]}, год: {book["year"]}, статус: {book["status"]}',
-                sep='\n')
+                f'id: {book["id"]}, название: {book["title"]}, автор: {book["author"]}, год: {book["year"]}, статус: {book["status"]}'
+            )
         main()
 
     def change_book_status(self):
         id_book = input('Введите идентификатор книги: ').strip()
-        for book in self.books:
-            if book['id'] == id_book:
-                print('1 - в наличии', '2 - выдана', sep='\n')
-                new_status = input('Введите статус книги: ').strip()
-                if new_status == '1':
-                    book['status'] = 'в наличии'
-                elif new_status == '2':
-                    book['status'] = 'выдана'
-                else:
-                    print('Неверный ввод!')
-                self.save_db()
-                print(f'Статус книги "{book["title"]}" изменен на "{book["status"]}"')
+        book = next((b for b in self.books if b['id'] == id_book), None)
+        if book:
+            print('1 - в наличии', '2 - выдана', sep='\n')
+            new_status = input('Введите статус книги: ').strip()
+            if new_status == '1':
+                book['status'] = 'в наличии'
+            elif new_status == '2':
+                book['status'] = 'выдана'
+            else:
+                print('Неверный ввод!')
                 return main()
+            self.save_db()
+            print(f'Статус книги "{book["title"]}" изменен на "{book["status"]}"')
+        else:
             print('Книга с таким идентификатором не найдена!')
+        main()
+
+    def input_year(self):
+        while True:
+            year = input('Введите год издания книги: ').strip()
+            if year.isdigit() and len(year) == 4:
+                return year
+            print('Неверный ввод! Год издания должен быть числом из 4 цифр.')
 
 
 def main():
     db_file_name = 'library.json'
     lib = LibraryManagement(db_file_name)
-    print('1 - Добавить книгу', '2 - Удать книгу', '3 - Найти книгу', '4 - Отобразить все книги',
+    print('1 - Добавить книгу', '2 - Удалить книгу', '3 - Найти книгу', '4 - Отобразить все книги',
           '5 - Изменить статус книги', sep='\n')
     action = input('Введите действие: ')
     if action == '1':
